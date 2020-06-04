apluslms/service-base

commit 8c1622e3ccfc500829d9677a5ba80ffc1abd426b

Replace exports with env, so variables are passed only to one command

dbipc/rootfs/usr/local/sbin/create-db.sh
@@ -27,10 +27,12 @@ setuidgid postgres createdb -O "$user" "$db"
 
 # Initialize the database
 if [ $# -gt 0 ]; then
-    export DATABASE_USER="$user"
-    export DATABASE_NAME="$db"
-    export DATABASE_IS_EMPTY=true
-    setuidgid "$user" "$@" 2>&1
+    exec env \
+        USER="$user" \
+        DATABASE_USER="$user" \
+        DATABASE_NAME="$db" \
+        DATABASE_IS_EMPTY=true \
+        setuidgid "$user" "$@"
 fi
 
 # Dump the database

commit 9efaa96e23202fb264c21be6e5ad38bb5de23ace

Fix create-db.sh, it exited and crashed the db
This fixes a regression bug introduced in 8c1622e. Earlier commit
used `exec`, which meant that the database was not killed gracefully,
which mean the restart of the database started a slow recovery process.
Eventually, this meant that the service container failed to start.

dbipc/rootfs/usr/local/sbin/create-db.sh
@@ -27,8 +27,7 @@ setuidgid postgres createdb -O "$user" "$db"
 
 # Initialize the database
 if [ $# -gt 0 ]; then
-    exec env \
-        USER="$user" \
+    env USER="$user" \
         DATABASE_USER="$user" \
         DATABASE_NAME="$db" \
         DATABASE_IS_EMPTY=true \